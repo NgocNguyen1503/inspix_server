bfb33af84f756ea3e4bc8633a9f7d41ebdb75a7b
NgocNguyen1503 <136145943+NgocNguyen1503@users.noreply.github.com>
2026-04-15 23:17:24 +0700

    Add migrations

---
+++ database/migrations/2026_04_15_000130_create_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->string('uuid', 255)->primary();
+            $table->string('color', 50)->nullable();
+            $table->text('url_small');
+            $table->text('url_regular');
+            $table->text('url_full');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('collection_id');
+            $table->text('download_url')->nullable();
+            $table->unsignedInteger('total_views')->default(0);
+            $table->unsignedInteger('total_likes')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+    }
+};
